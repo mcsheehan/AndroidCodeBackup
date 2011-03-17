23f24a35776489638bdf668cdadab692522943fd
mcs <mcs@robots.ox.ac.uk>
2011-03-17 15:55:08 +0000

    Working android application, with all of the buttons displaying the correct text as well as the screen location printing as x and y values, hooray!

--- gen/mcs/MediaRemote/R.java
+++ gen/mcs/MediaRemote/R.java
@@ -13,6 +13,14 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int DebugText=0x7f050001;
+        public static final int FastForward=0x7f050002;
+        public static final int Play=0x7f050000;
+        public static final int Power=0x7f050005;
+        public static final int Rewind=0x7f050003;
+        public static final int Stop=0x7f050004;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
--- src/mcs/MediaRemote/RemoteScreen.java
+++ src/mcs/MediaRemote/RemoteScreen.java
@@ -2,12 +2,65 @@ package mcs.MediaRemote;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
-public class RemoteScreen extends Activity {
-    /** Called when the activity is first created. */
+
+public class RemoteScreen extends Activity 
+{
+	private TextView DebugTextBox;
+	
+	/** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) 
+    {
+       	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        DebugTextBox = (TextView) findViewById(R.id.DebugText);
     }
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		float XValue = event.getRawX();
+		float YValue = event.getRawY();
+		
+		String TouchData = "X= " + Float.toString(XValue) + " Y= " + Float.toString(YValue);
+		DebugTextBox.setText(TouchData);
+		
+		return super.onTouchEvent(event);
+	}
+
+
+
+	public void PlayListener(View PresentView)
+	{
+		DebugTextBox.setText("Play button");
+	}
+	public void FastForwardListener(View PresentView)
+	{
+//		TextView DebugTextBox = (TextView) findViewById(R.id.DebugText);
+		DebugTextBox.setText("FF button");
+	}
+	public void RewindListener(View PresentView)
+	{
+//		TextView DebugTextBox = (TextView) findViewById(R.id.DebugText);
+		DebugTextBox.setText("Rewind button");
+	}
+	public void StopListener(View PresentView)
+	{
+//		TextView DebugTextBox = (TextView) findViewById(R.id.DebugText);
+		DebugTextBox.setText("Stop button");
+	}
+	public void PowerListener(View PresentView)
+	{
+//		TextView DebugTextBox = (TextView) findViewById(R.id.DebugText);
+		DebugTextBox.setText("Power button");
+	}
+
+	public void FingerTracker(View PresentView)
+	{
+		
+	}
 }
